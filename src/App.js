50ae2255e38bd77203bb553aba6ad3f1e26fa15b
pc <shaimaabouassab@gmail.com>
2023-08-19 14:29:31 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
+      
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
